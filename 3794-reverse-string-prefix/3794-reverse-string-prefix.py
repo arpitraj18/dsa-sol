640ab228ada31ccd16a879469a172b0d8b06842b
Arpit Raj <arpitimp18@gmail.com>
2026-07-17 00:22:46 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.98%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        s=list(s)
+        left=0
+        right=k-1
+
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        return "".join(s)
+        
